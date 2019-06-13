e76e6845bfdaaf464c50648549d15b13db726080
Anna Kulikowska <maryann6@o2.pl>
2019-06-13 14:07:58 +0200

    initital commit

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('siema! Skąd się tu wziąłeś drogi czytelniku?');
+const name = 'Ania';
+const age = 36;
+console.log('name');
+console.log('age');
+console.log(`Nazywam się ${name} i mam ${age} lat`);
