dd289d4ba4cde1e407ecaeea3731e4a75e39a26f
Anna Kulikowska <maryann6@o2.pl>
2019-07-04 15:33:44 +0200

    css changes

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ const age = 36;
 console.log('name');
 console.log('age');
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+const mainHeader = document.querySelector('.mainHeader');
+
+console.log(mainHeader);
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+const header = document.querySelector('.mainHeader');
+function calculate (x){
+    x=x+3
+    console.log (x)
+    return x*7
+}
+calculate(1);
